71454cacbc6b6a721b9c345dbeb7850ed6225996
Oscar Wales <oscarjwales@gmail.com>
2020-09-28 17:50:12 +0100

    Replaced filler text, reformatted 'about' section to have even spacing between sections. Changed position properties of some elements so scroll arrows will stick to bottom/top of each section.

--- src/components/About.js
+++ src/components/About.js
@@ -3,17 +3,21 @@ import bottomArrow from "../styles/assets/icons/double-arrow-bottom.svg"
 
 const About = () => (
     <>
-        <section class="jumbo">
+        <article class="jumbo">
             <ul class="navList">
                 <li class="navLink">Home</li>
                 <li class="navLink">Projects</li>
                 <li class="navLink">Contact</li>
             </ul>
-            <h1 class="heading">Oscar Wales</h1>
-            <h2 class="subHeading">Junior Developer</h2>
-            <p class="plainText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates assumenda doloremque nesciunt sapiente id molestias, vero eum, esse quisquam minima quam tenetur veniam! Accusantium ipsum placeat similique distinctio, repellendus qui?</p>
-            <img class="scrollWhite" src={bottomArrow} alt="downwards pointing arrow"/>
-        </section>
+            <section>
+                <h1 class="heading">Oscar Wales</h1>
+                <h2 class="subHeading">Junior Developer</h2>
+            </section>
+            <section>
+                <p class="plainText description">I'm a recent graduate of Develop Me's Coding Fellowship Bootcamp with a passion for building beautiful webistes and apps using tidy and efficent code.</p>
+                <img class="scrollWhite down" src={bottomArrow} alt="downwards pointing arrow"/>
+            </section>
+        </article>
     </>
 );
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import scrollUp from "../styles/assets/icons/double-arrow-top.svg";
 import scrollDown from "../styles/assets/icons/double-arrow-bottom.svg";
 import pingPong from "../styles/assets/images/pong.png";
+import nerd from "../styles/assets/images/nerd.png";
 
 const Projects = () => (
     <>
@@ -11,18 +12,30 @@ const Projects = () => (
             <section class="cardGroup">
             <article class="card">
                 <img class="cardImg" src={ pingPong } alt="Landing page of pong-themed ping-pong tournament app" />
-                <h3 class="cardHeading">Project 1</h3>
-                <p class="cardText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis assumenda tenetur, repellat itaque eligendi mollitia modi natus voluptate similique beatae quod debitis est aspernatur quas distinctio aliquid, magnam quia. Fugiat?</p>
+                <h3 class="cardHeading">(Ping)-Pong</h3>
+                <p class="cardText">Retro Pong-themed tool that randomly created pairings for a table tennis tournament bracket from a list of names collected from the user. UI built using React and Redux for state management.</p>
+                <ul class="cardLinks">
+                    <li class="cardLinkText"><a href="https://github.com/LaikaTheSpaceDog/pong">Repo</a></li>
+                    <li class="cardLinkText"><a href="https://laikathespacedog.github.io/pong/#/">Live</a></li>
+                </ul>
+            </article>
+            <article class="card">
+                <img class="cardImg" src={ nerd } alt="Landing page of Nerd Words" />
+                <h3 class="cardHeading">Nerd Words</h3>
+                <p class="cardText">This dictionary app for beginners to programming was built as a group project. I was responsible for the the back end of this group project that I built using Laravel and AWS.</p>
+                <ul class="cardLinks">
+                    <li class="cardLinkText"><a href="https://github.com/LaikaTheSpaceDog/final-project-back-end">Repo</a></li>
+                    <li class="cardLinkText"><a href="https://alexanderbraatz.github.io/final-project-front-end/#/words">Live</a></li>
+                </ul>
             </article>
             <article class="card">
                 <img class="cardImg" src="" alt="" />
-                <h3 class="cardHeading">Project 1</h3>
-                <p class="cardText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis assumenda tenetur, repellat itaque eligendi mollitia modi natus voluptate similique beatae quod debitis est aspernatur quas distinctio aliquid, magnam quia. Fugiat?</p>
-            </article>
-            <article class="card">
-                <img class="cardImg" src="" alt="" />
-                <h3 class="cardHeading">Project 1</h3>
-                <p class="cardText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis assumenda tenetur, repellat itaque eligendi mollitia modi natus voluptate similique beatae quod debitis est aspernatur quas distinctio aliquid, magnam quia. Fugiat?</p>
+                <h3 class="cardHeading">TBC</h3>
+                <p class="cardText"></p>
+                <ul class="cardLinks">
+                    <li class="cardLinkText"><a>Repo</a></li>
+                    <li class="cardLinkText"><a>Live</a></li>
+                </ul>
             </article>
             </section>
             <img class="scroll" src={ scrollDown } alt="Scroll down arrow" /> 
