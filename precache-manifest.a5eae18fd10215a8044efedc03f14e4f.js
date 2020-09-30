ff404cc855d148859755b9a0cc1320cfae8af9a4
Oscar Wales <oscarjwales@gmail.com>
2020-09-30 16:05:00 +0100

    Updates

---
+++ precache-manifest.a5eae18fd10215a8044efedc03f14e4f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e3be3820f42c61d1820bc5096f7d0b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e61845546ba4ab5dc9e9",
+    "url": "/portfolio/static/css/main.11b2a59e.chunk.css"
+  },
+  {
+    "revision": "fbb7697eb2dd7904e4ae",
+    "url": "/portfolio/static/js/2.d66bdd12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.d66bdd12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61845546ba4ab5dc9e9",
+    "url": "/portfolio/static/js/main.22f1ff8f.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "b1ea8d6c59804396fdeb6f8ff0092cac",
+    "url": "/portfolio/static/media/NoticiaText-Regular.b1ea8d6c.ttf"
+  },
+  {
+    "revision": "0aaab6d017f51555b3a5612d1022f51e",
+    "url": "/portfolio/static/media/double-arrow-bottom.0aaab6d0.svg"
+  },
+  {
+    "revision": "0e07576448299396d5a2699e9b0e221d",
+    "url": "/portfolio/static/media/double-arrow-top.0e075764.svg"
+  },
+  {
+    "revision": "5b09adca12c14e5973e2d4ea07100ff8",
+    "url": "/portfolio/static/media/envelope-line.5b09adca.svg"
+  },
+  {
+    "revision": "a9f7ad3dc6d998df5c984f5363913c7f",
+    "url": "/portfolio/static/media/github.a9f7ad3d.svg"
+  },
+  {
+    "revision": "e7ccc098fdee284ae94ea7acda096f3a",
+    "url": "/portfolio/static/media/linkedin-square.e7ccc098.svg"
+  },
+  {
+    "revision": "5c88f7f5a986726332fdf3030791c613",
+    "url": "/portfolio/static/media/nerd.5c88f7f5.png"
+  },
+  {
+    "revision": "0981d1ab9bdfe7e2c8d61ca40222ed8f",
+    "url": "/portfolio/static/media/ostrich-sans-black.0981d1ab.ttf"
+  },
+  {
+    "revision": "468c84aadb77420020c28c76a44b5e58",
+    "url": "/portfolio/static/media/ostrich-sans-black.468c84aa.woff"
+  },
+  {
+    "revision": "60eb852ee4209a8ae6bcc604198de68f",
+    "url": "/portfolio/static/media/ostrich-sans-regular.60eb852e.ttf"
+  },
+  {
+    "revision": "cd994a865fca5d7aa16394f2f572ae4d",
+    "url": "/portfolio/static/media/ostrich-sans-regular.cd994a86.woff"
+  },
+  {
+    "revision": "bd97ab0b892bc026d6f643bf8b786b19",
+    "url": "/portfolio/static/media/pet.bd97ab0b.png"
+  },
+  {
+    "revision": "148d1ec667e692c0fdb4a637c6f26b2c",
+    "url": "/portfolio/static/media/pong.148d1ec6.png"
+  }
+]);
