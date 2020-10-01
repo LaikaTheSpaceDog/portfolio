bc697a071376f6fbbb5852f9935ace1affe3cf09
Oscar Wales <oscarjwales@gmail.com>
2020-10-01 12:19:14 +0100

    Added smooth scrolling on links, fixed email icon href, fixed scroll button on contact so moved to proj 3 when cards are seperated and projs when together

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -8,16 +8,17 @@ import Footer from "./Footer";
 const Contacts = () => (
     <>
         <article className="contact snap" id="contact">
-        <a href="#project3"><img className="scrollWhite up" src={ scrollUp } alt="Scroll up arrow" /></a>
+        <a href="#projects"><img className="scrollWhite up projs" src={ scrollUp } alt="Scroll up arrow" /></a>
+        <a href="#proj3"><img className="scrollWhite up proj3" src={ scrollUp } alt="Scroll up arrow" /></a>
             <section className="contactNav">
                 <h1 className="heading">Contact</h1>
                 <section className="contactBar">
-                    <a href="https://github.com/LaikaTheSpaceDog"><img className="contactIcon" src={ github } alt="GitHub logo"/></a>
+                    <a href="https://github.com/LaikaTheSpaceDog" target="_blank" rel="noopener noreferrer"><img className="contactIcon" src={ github } alt="GitHub logo"/></a>
                     <div className="email">
-                        <a href="oscarjwales@gmail.com"><img className="contactIcon" src={ email } alt="Email icon" /></a>
+                        <a href="mailto:oscarjwales@gmail.com" target="_blank" rel="noopener noreferrer"><img className="contactIcon" src={ email } alt="Email icon" /></a>
                         <p className="plainText">oscarjwales@gmail.com</p>
                     </div>
-                    <a href="https://www.linkedin.com/in/oscar-wales/"><img className="contactIcon" src={ linkedin } alt="LinkedIn logo"/></a>
+                    <a href="https://www.linkedin.com/in/oscar-wales/" target="_blank" rel="noopener noreferrer"><img className="contactIcon" src={ linkedin } alt="LinkedIn logo"/></a>
                 </section>
             </section>
             <Footer />
