8c3e825370a4ea85326ca5db1efd47cd5b888344
Oscar Wales <oscarjwales@gmail.com>
2021-04-24 17:23:57 +0100

    Some changes to jumbo styling and media queries. Changed footer to use current year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
     <footer className="footer">
-        <p className="footerText">&#x24B8; Oscar Wales 2020</p>
+        <p className="footerText">&#x24B8; Oscar Wales {new Date().getFullYear()}</p>
     </footer>
 );
 
